bbdc3686730227425149f8ba10b94ffb1aadadc4
Anis Sadykov <sadikowas@yandex.ru>
2022-11-11 19:10:49 +0300

    task 1

---
+++ Week_10/HttpServer/HttpServer/Controllers/Accounts.cs
@@ -0,0 +1,28 @@
+﻿using HttpServer.Attributes;
+using HttpServer.Models;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using HttpServer.ORM.DAO;
+
+namespace HttpServer.Controllers
+{
+    [HttpController("accounts")]
+    public class Accounts
+    {
+
+        private AccountDAO _accountDAO =
+            new AccountDAO("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SteamDB;Integrated Security=True;");
+        [HttpGET]
+        public List<Account> GetAccounts() =>
+            _accountDAO.GetAll();
+
+        [HttpGET("\\d+")]
+        public Account? GetAccountById(int id) =>
+            _accountDAO.GetEntityByKey(id);
+
+        [HttpPOST]
+        public void SaveAccount(string login, string password) =>
+            _accountDAO.Create(new Account(login, password));
+    }
+}
